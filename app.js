2a4ae500ed3f05aa2d4e25b28e31d674024a9553
AlexeyT74 <alexey.tal@gmail.com>
2024-03-26 21:58:22 +0200

    Beta-3 Multi Worlds

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
 // Constants
 
-const WS_WIDTH = 10;
-const WS_HEIGHT = 8;
+// const WS_WIDTH = 10;
+// const WS_HEIGHT = 8;
 
-// translate these constants into CSS
-document.documentElement.style.setProperty("--ws-height", WS_HEIGHT);
-document.documentElement.style.setProperty("--ws-width", WS_WIDTH);
-document.documentElement.style.setProperty("--ws-height-fr", `${WS_HEIGHT}fr`);
-document.documentElement.style.setProperty("--ws-width-fr", `${WS_WIDTH}fr`);
+// // translate these constants into CSS
+// document.documentElement.style.setProperty("--ws-height", WS_HEIGHT);
+// document.documentElement.style.setProperty("--ws-width", WS_WIDTH);
+// document.documentElement.style.setProperty("--ws-height-fr", `${WS_HEIGHT}fr`);
+// document.documentElement.style.setProperty("--ws-width-fr", `${WS_WIDTH}fr`);
 
-const INI_WORLD = [
-  // Initial state of the world
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 1, 3, 1, 1, 2, 2, 2],
-  [0, 0, 1, 3, 3, 3, 2, 2, 2, 0],
-  [0, 3, 3, 3, 0, 0, 0, 2, 2, 0],
-  [1, 3, 3, 1, 1, 0, 0, 2, 2, 2],
-  [1, 1, 3, 3, 1, 1, 0, 0, 2, 2],
-  [3, 3, 3, 1, 0, 0, 0, 2, 2, 0],
-  [3, 3, 1, 1, 1, 1, 2, 2, 0, 0],
+let worldType = 0; //First option of the world grid
+
+const init = [
+  {
+    name: "Generic",
+    width: 10,
+    height: 8,
+    grid: [
+      // Initial state of the world
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 3, 1, 1, 2, 2, 2],
+      [0, 0, 1, 3, 3, 3, 2, 2, 2, 0],
+      [0, 3, 3, 3, 0, 0, 0, 2, 2, 0],
+      [1, 3, 3, 1, 1, 0, 0, 2, 2, 2],
+      [1, 1, 3, 3, 1, 1, 0, 0, 2, 2],
+      [3, 3, 3, 1, 0, 0, 0, 2, 2, 0],
+      [3, 3, 1, 1, 1, 1, 2, 2, 0, 0],
+    ],
+  },
+  {
+    name: "Water World",
+    width: 15,
+    height: 8,
+    grid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0],
+      [0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 1, 3, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 0],
+      [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  },
+  {
+    name: "Circle",
+    width: 10,
+    height: 10,
+    grid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+      [0, 0, 1, 1, 0, 0, 3, 3, 0, 0],
+      [0, 1, 3, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 1, 3, 3, 1, 0, 0, 0],
+      [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  },
 ];
 
 const logic = {
@@ -39,7 +80,7 @@ const logic = {
       id: 2,
       name: "Rock",
       under: 0, // Water fills the tile when Rock is extracted
-      style: "rock",      
+      style: "rock",
     },
     {
       id: 3,
@@ -71,11 +112,26 @@ const current = {
 };
 
 // Prepare the world
-initTools();
+initChangeWorld();
 initWorld();
-initResetButton();
+initTools();
 
 // General functions
+
+function initConstants() {
+  const select = document.querySelector("select.world_type");
+  worldType = (Number)(select.value);
+
+  // translate these constants into CSS
+  document.documentElement.style.setProperty("--ws-height", init[worldType].height);
+  document.documentElement.style.setProperty("--ws-width", init[worldType].width);
+  document.documentElement.style.setProperty(
+    "--ws-height-fr",
+    `${init[worldType].height}fr`
+  );
+  document.documentElement.style.setProperty("--ws-width-fr", `${init[worldType].width}fr`);
+}
+
 function initTools() {
   console.log("Init Tools");
   const ul = document.querySelector("ul.tools");
@@ -86,12 +142,26 @@ function initTools() {
   }
 }
 
-function initResetButton() {
+function initChangeWorld() {
   const button = document.querySelector("input.reset");
   button.addEventListener("click", initWorld);
+
+  const select = document.querySelector("select.world_type");
+  while (select.firstElementChild){
+    select.removeChild(select.firstElementChild);
+  }
+  for(let i=0; i<init.length; i++){
+    const option = document.createElement("option");
+    option.value = i;
+    option.innerText = init[i].name;
+    select.appendChild(option);    
+  }
+  select.value = "0";
 }
 
-function initWorld() {
+function initWorld() {  
+  initConstants();
+
   console.log("Draw Inner Grid");
 
   // Redraw World (tiles) part
@@ -102,10 +172,10 @@ function initWorld() {
   }
 
   // reinit World array with initial values
-  current.world.push(...INI_WORLD);
+  current.world = [...init[worldType].grid];
 
   // Draw required number of rows
-  for (let i = 0; i < WS_HEIGHT; i++) {
+  for (let i = 0; i < init[worldType].height; i++) {
     const row = createRow(i);
     container.appendChild(row);
   }
@@ -115,7 +185,7 @@ function initWorld() {
     row.className = "row";
 
     //Add required number of tiles to the row
-    for (let j = 0; j < WS_WIDTH; j++) {
+    for (let j = 0; j < init[worldType].width; j++) {
       const tile = createTile(rowIndex, j);
       row.appendChild(tile);
     }
@@ -127,7 +197,6 @@ function initWorld() {
       const logicTile = logic.tiles[current.world[row][col]];
       drawTile(tile, logicTile);
       tile.addEventListener("click", removeElement);
-      // tile.dataset.id = current.world[row][col];
       return tile;
     }
   }
@@ -144,7 +213,7 @@ function initWorld() {
   current.inventory.splice(0);
 
   // Draw required number of inventory items
-  for (let i = 0; i < WS_HEIGHT; i++) {
+  for (let i = 0; i < init[worldType].height; i++) {
     const inv_item = createInventoryEmptyItem();
     inventoryContainer.appendChild(inv_item);
   }
@@ -153,7 +222,6 @@ function initWorld() {
 function createInventoryEmptyItem() {
   const item = document.createElement("div");
   item.classList.add("inv-item", "box");
-  // item.innerText = "empty";
   return item;
 }
 
@@ -177,7 +245,7 @@ function removeElement(elem) {
     return; // Tool is not compatible with the terrain type
   }
 
-  if (pushInventory(tileElem, tile) && (tile.under>=0)) {
+  if (pushInventory(tileElem, tile) && tile.under >= 0) {
     // remove the tile from the world;
     const newTile = logic.tiles[tile.under];
     drawTile(tileElem, newTile);
@@ -221,7 +289,7 @@ function pushInventory(elem, tile) {
     `Inserting into inventory: ${tile.name}, size of the inventory: ${current.inventory.length}`
   );
 
-  if (current.inventory.length >= WS_HEIGHT) {
+  if (current.inventory.length >= init[worldType].height) {
     alert("The Inventory is full!");
     return false;
   }
